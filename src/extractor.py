a9e808206b4f31cde75f48e2281ecba08c226e3d
maksimprilepsky@yandex.ru <maksimprilepsky@yandex.ru>
2023-01-03 13:17:06 +0400

    add extract text and extract sentences methods

--- src/extractor.py
+++ src/extractor.py
@@ -1,41 +1,96 @@
 from typing import List
 
+from razdel import sentenize
+
 from html_parser import CustomHTMLParser
 
 
 class Extractor:
-    def __init__(self, separator: str = '\n'):
+    def __init__(self,
+                 html: str,
+                 separator: str = '|separator|',
+                 inner_tags: List[str] = None,
+                 outer_tags: List[str] = None,
+                 non_processing_tags: List[str] = None):
         """
+            @param html : str
+                html in string wich you want to parse
             @param separator : str
                 Example:
                     INCOMMING HTML - '<h1>First text</h1><h2>Second text</h2>'
                     
-                    WITH SEPARATOR - 'First text\nSecond text'
+                    WITH SEPARATOR - 'First text|separator|Second text'
                     WITHOUT SEPARATOR - 'First textSecond text'
         """
-        self.parser = CustomHTMLParser()
+        custom_html_parser_kwargs = {}
+        assert isinstance(html, str)
+        if inner_tags:
+            assert isinstance(inner_tags, list) or isinstance(inner_tags, tuple)
+            custom_html_parser_kwargs['inner_tags'] = inner_tags
+        if outer_tags:
+            assert isinstance(outer_tags, list) or isinstance(outer_tags, tuple)
+            custom_html_parser_kwargs['outer_tags'] = outer_tags
+        if non_processing_tags:
+            assert isinstance(non_processing_tags, list) or isinstance(non_processing_tags, tuple)
+            custom_html_parser_kwargs['non_processing_tags'] = non_processing_tags
+        self.parser = CustomHTMLParser(**custom_html_parser_kwargs)
+        self.entities = self.parser.get_entities(data=html)
         self.separator = separator
         
-    def __concatinate_datas(self, entities: List[tuple]) -> str:
+    def __concatinate_datas(self, entities: List[tuple], separator: str) -> str:
         """
             @param entities : List[tuple]
                 list of tuple of entities in html data
                 
             @return extracted text from html
         """
-        filtered_datas = list(filter(lambda x: x[0] == 'data', entities))
-        if self.separator:
-            return f'{self.separator}'.join([x[1] for x in filtered_datas])
-        else:
-            return ''.join([x[1] for x in filtered_datas])
-        
-    def extract_text(self, data: str) -> str:
+        text = ''
+        last_tag_is_outer = False
+        for entitie_elem in entities:
+            if entitie_elem[0] == 'data':
+                if last_tag_is_outer and len(text) != 0 and separator:
+                    text += separator
+                text += entitie_elem[1]   
+            if entitie_elem[0] == 'tag' and entitie_elem[2] == 'outer':
+                last_tag_is_outer = True
+            else:
+                last_tag_is_outer = False 
+            
+        return text
+    
+    def __sentenize(self, text_list: List[str]) -> List[str]:
         """
-            @param data : str
-                html data in string
+            @param text_list : List[str]
+                List of splited text by separator
                 
+            @return List[str]
+                List of sentences sentenized by razdel
+        """
+        sentences = []
+        for text_elem in text_list:
+            sentences += [x.text for x in list(sentenize(text_elem))]
+            
+        return sentences
+        
+    def extract_text(self) -> str:
+        """     
             @return str
                 extracted text from html
         """
-        entities = self.parser.get_entities(data=data)
-        return self.__concatinate_datas(entities=entities)
+        
+        return self.__concatinate_datas(entities=self.entities, separator=self.separator)
+    
+    def extract_sentences(self) -> str:
+        """
+            @return List[str]
+                List of sentences sentenized by razdel
+        """
+        if self.separator:
+            separator = self.separator
+        else:
+            separator = '|separator|'
+            
+        text = self.__concatinate_datas(entities=self.entities, separator=separator)
+        text_list = text.split(separator)
+        
+        return self.__sentenize(text_list=text_list)
--- src/html_parser.py
+++ src/html_parser.py
@@ -2,7 +2,7 @@ from typing import List
 
 from html.parser import HTMLParser
 
-from const import NON_PROCESSING_TAGS, INNER_TAGS, OUTER_TAGS, LINE_BREAK_SYMBOLS
+from const import NON_PROCESSING_TAGS, INNER_TAGS, OUTER_TAGS
 
 
 class CustomHTMLParser(HTMLParser):
@@ -12,23 +12,16 @@ class CustomHTMLParser(HTMLParser):
         This class contains rewrated methods of standart HTMLParser class.
         This class parses html and creates a list of entities (tags, scripts, data) for further use by child classes.
     """
-    def __init__(self):
+    def __init__(self, inner_tags: List[str] = INNER_TAGS, outer_tags: List[str] = OUTER_TAGS, non_processing_tags: List[str] = NON_PROCESSING_TAGS):
         super().__init__()
         self.all_html_entities = []
+        self.inner_tags = inner_tags
+        self.outer_tags = outer_tags
+        self.non_processing_tags = non_processing_tags
         self.last_tag_in_non_processing = False
         
-    def __validate_incomming_data(self, data):
-        """
-            @param data : str
-                html data as a string
-                
-            Func for check incomming data type is str or not. Parser will works only with string
-        """
-        if not isinstance(data, str):
-            raise Exception('Make sure you are trying to parse string')
-        
     def handle_starttag(self, tag, attrs):
-        if tag in NON_PROCESSING_TAGS:
+        if tag in self.non_processing_tags:
             self.last_tag_in_non_processing = True
         else:
             self.last_tag_in_non_processing = False
@@ -36,18 +29,18 @@ class CustomHTMLParser(HTMLParser):
         for attr in attrs:
             tag_attrs.append(f'{attr[0]}="{attr[1]}"')
         tag_str = f'<{tag} {" ".join(tag_attrs)}>' if len(tag_attrs) != 0 else f'<{tag}>'
-        if tag in INNER_TAGS:
+        if tag in self.inner_tags:
             data_to_append = ('tag', tag_str, 'inner')
-        elif tag in OUTER_TAGS:
+        elif tag in self.outer_tags:
             data_to_append = ('tag', tag_str, 'outer')
         else:
             data_to_append = ('tag', tag_str, 'other')
         self.all_html_entities.append(data_to_append)
         
     def handle_endtag(self, tag):
-        if tag in INNER_TAGS:
+        if tag in self.inner_tags:
             data_to_append = ('tag', f'</{tag}>', 'inner')
-        elif tag in OUTER_TAGS:
+        elif tag in self.outer_tags:
             data_to_append = ('tag', f'</{tag}>', 'outer')
         else:
             data_to_append = ('tag', f'</{tag}>', 'other')
@@ -55,12 +48,12 @@ class CustomHTMLParser(HTMLParser):
         
     def handle_data(self, data):
         if not self.last_tag_in_non_processing:
-            self.all_html_entities.append(('data', data))
+            self.all_html_entities.append(('data', data, 'data'))
         else:
             self.all_html_entities.append(('tag', data, 'other'))
             
     def get_entities(self, data: str) -> List[tuple]:
-        self.__validate_incomming_data(data=data)
+        assert isinstance(data, str)
         self.feed(data=data)
         
         return self.all_html_entities
